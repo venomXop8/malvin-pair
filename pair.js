5a0ceb753a1bbfbe2951c92e3df4ba403605d636
IRONMAN x VENOM <gautamdipanshu475@gmail.com>
2025-07-12 14:13:50 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,36 +75,26 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "malvin~" + string_session;
+                        let md = "cybro~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, MALVIN-XD User!* 👋🏻
+                        let desc = `*Hey there, CYBRO-MD User!* 👋🏻
 
-Thanks for using *MALVIN-XD* — your session has been successfully created!
+Thanks for using *CYBRO-MD* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
-⚠️ *Keep it safe!* Do NOT share this ID with anyone.
+⚠️ *Keep it safe!* Do NOT share this ID with anyone
 
 ——————
 
-*✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
-
-*💻 Source Code:*  
-Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
-
-——————
-
-> *© Powered by Malvin King*
+> *© Powered by Ironman King*
 Stay cool and hack smart. ✌🏻`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "ᴍᴀʟᴠɪɴ-xᴅ",
-thumbnailUrl: "https://files.catbox.moe/bqs70b.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A",
+title: "cybro-md",
+thumbnailUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-kjFSvHqteCc2NmeawKR0rgf-M0Icv_-JTwXGgru9bjKgPTI&s",
+sourceUrl: "https://whatsapp.com/channel/0029VbACIcr4dTnAIO4gWP3y",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -113,34 +103,24 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `Hey there, MALVIN-XD User!* 👋🏻
+                            let desc = `Hey there, CYBRO-MD User!* 👋🏻
 
-Thanks for using *MALVIN-XD* — your session has been successfully created!
+Thanks for using *CYBRO-MD* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
 
 ——————
 
-*✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A
-
-*💻 Source Code:*  
-Fork & explore the project on GitHub:  
-https://github.com/XdKing2/MALVIN-XD
-
-——————
-
-> *© Powered by Malvin King*
+> *© Powered by Ironman King*
 Stay cool and hack smart. ✌🏻`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "ᴍᴀʟᴠɪɴ-xᴅ",
-thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
-sourceUrl: "https://whatsapp.com/channel/0029VbA6MSYJUM2TVOzCSb2A",
+title: "Cybro-md",
+thumbnailUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-kjFSvHqteCc2NmeawKR0rgf-M0Icv_-JTwXGgru9bjKgPTI&s",
+sourceUrl: "https://whatsapp.com/channel/0029VbACIcr4dTnAIO4gWP3y",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
